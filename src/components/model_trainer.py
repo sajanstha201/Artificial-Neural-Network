b396e7f56107e2414bdcdb14e703219cfba7b997
sajan shrestha <sajanshrestha@sajan-shrestha.local>
2024-01-17 22:12:12 +0530

    added some hyperparamter for our machine learning algorithms for more accuracy

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -36,6 +36,47 @@ class model_trainer:
                 "Random Forest":RandomForestClassifier()
             }
             logging.info("loaded the required model")
+            params={
+                "Logistic Regression":{
+                    'C':[0.1,1,10],
+                    #'solver':['lbfgs','liblinear','sag','saga'],
+                    'max_iter':[100,500,1000,50000]
+                    },
+                "SVM":{
+                    #'C':[0.01,0.1,1,10,10,100],
+                    #'kernel':['linear','rbf','poly','sigmoid'],
+                    #'gamma':[0.01,0.1,1,10,100],
+                    #'degree':[2,3,4,5]
+                    },
+                "Naive Bayes":{
+                    #'alpha':[0.001,0.01,0.1,1,10,100]
+                    },
+                "KNN":{
+                    #'n_neighbors':[5,6,7,8],
+                    #'metric':['euclidean','manhattan','minkowski'],
+                    #'algorithm':['auto','ball_tree','kd_tree'],
+                    #'leaf_size':[10,20]
+                    },
+                "K Mean":{
+                    'n_init':['auto']
+                    #'n_clusters':[2,3,4,5,6,7],
+                    #'max_iter':[100,200,300],
+                    },
+                "Decision Tree":{
+                    #'criterion':['gini','entropy','squared_error','absolute_error','poisson'],
+                    #'splitter':['random','best'],
+                    #'max_depth':[100,200,300],
+                    #'min_samples_split':[1,2,3,4,5],
+                   #'min_samples_leaf':[1,2,3,4,5],
+                    #'max_features':[]
+                    },
+                "Random Forest":{
+                    #'criterion':['gini','entropy','squared_error','absolute_error','poisson'],
+                    #'splitter':['random','best'],
+                    #n_estimators': [8,16,32,64,128,256]
+                }
+            }
+            logging.info("declared the hyperparameter for each model")
             x_train,y_train,x_test,y_test=(
                 train_arr[:,:-1],
                 train_arr[:,-1],
@@ -48,7 +89,8 @@ class model_trainer:
                 y_train=y_train,
                 x_test=x_test,
                 y_test=y_test,
-                models=models
+                models=models,
+                params=params
             )
             logging.info("got the model report of each model")
             best_model_score=np.max(list(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 from src.exception import customException
 from src.logger import logging
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
 def save_obj(obj_path,obj):
     try:
         os.makedirs(os.path.dirname(obj_path),exist_ok=True)
@@ -14,11 +15,20 @@ def save_obj(obj_path,obj):
     except Exception as e:
         logging.info(e)
         raise customException(e,sys)
-def model_evaluate(x_train,y_train,x_test,y_test,models):
-    accuracy=models.copy()
-    for model_name,model in models.items():
-        model.fit(x_train,y_train)
-        predict=model.predict(x_test)
-        accuracy.update({model_name:accuracy_score(y_test,predict)})
-    return accuracy
+def model_evaluate(x_train,y_train,x_test,y_test,models,params):
+    try:
+        report={}
+        for i in range(len(list(models.keys()))):
+            model=list(models.values())[i]
+            param=list(params.values())[i]
+            gs=GridSearchCV(estimator=model,param_grid=param,cv=5)
+            gs.fit(x_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+            predict=model.predict(x_test)
+            report[list(models.keys())[i]]=accuracy_score(y_test,predict)
+        return report
+    except Exception as e:
+        logging.info(e)
+        raise customException(e,sys)
         
