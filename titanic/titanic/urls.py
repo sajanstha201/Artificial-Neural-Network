f0d20967c7c10d5aae83cf799a319aeea9531710
sajan shrestha <sajanshrestha@sajan-shrestha.local>
2024-01-21 00:25:22 +0530

    again chaned the stru

--- titanic/titanic/urls.py
+++ titanic/titanic/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from app.views import home
 urlpatterns = [
+    path('',home),
     path('titanic/',home),
     path('admin/', admin.site.urls),
 ]
